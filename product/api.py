70d24c7218378a0c4bc5da61b8b0d3c1f4bf02d9
majid zarkesh <majidzarkesh@majids-MBP.fios-router.home>
2019-01-30 11:42:06 -0500

    first commit

--- product/api.py
+++ product/api.py
@@ -3,20 +3,16 @@
 # Import framework
 from flask import Flask
 from flask_restful import Resource, Api
-import redis
 
 # Instantiate the app
 app = Flask(__name__)
 api = Api(app)
-redis = redis(host='redis', port=6379)
-
 
 class Product(Resource):
     def get(self):
         return {
-            'products': ['Majid', 'John', 'David', 'Emily']
+            'products': ['John', 'Bob', 'Paddy', 'Aaron']
         }
-
 
 # Create routes
 api.add_resource(Product, '/')
--- website/index.php
+++ website/index.php
@@ -1,11 +1,30 @@
-
 <html>
     <head>
-        <title>My website </title>
+        <title>My Shop</title>
     </head>
 
     <body>
-        <h1>Welcome to my website</h1>
+        <h1>Welcome to my website </h1>
+        <h3>f5 demo </h3>
+
+        
+
+        <!-- Load icon library -->
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+<!-- The form -->
+<form class="example" action="Rediswebsite.py">
+  <input type="text" placeholder="Search List.." name="search">
+  <button type="submit"><i class="fa fa-search"></i></button>
+</form>
+
+<!-- The form -->
+<form class="example" action="Rediswebsite.py">
+  <input type="text" placeholder="add name to list.." name="search">
+  <button type="submit"><i class="fa fa-search"></i></button>
+</form>
+
+<h4> List of names in vip list</h4>
         <ul>
             <?php
             $json = file_get_contents('http://product-service/');
@@ -18,10 +37,4 @@
         </ul>
     </body>
 </html>
-
-
-
-
-
-
 
